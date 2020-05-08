e9b349cceca59c7b5d27a4c5f124b5eda27e7515
sevjaeg <sev.jaeger@gmail.com>
2020-05-08 21:54:44 +0200

    some stats added
    Signed-off-by: sevjaeg <sev.jaeger@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,6 @@ int main(int argc, char *argv[]) {
     int nthreads, tid;
 
     LockFreeList<int>* list = new LockFreeList<int>(INT32_MIN, INT32_MAX);
-    list->add(4);
-    list->add(4000);
-    list->remove(6);
-    list->print();
-    list->add(-4);
-    list->add(-40);
-    list->add(4000);
-    list->remove(4);
-    list->print();
-    list->contains(4);
-    list->contains(5);
 
     /*
     Node<int> n;
@@ -42,22 +31,65 @@ int main(int argc, char *argv[]) {
     std::cout << getFlag(nptr)<<"\n";
     */
 
-    const int items = 1000;
+    
+    double start_time, end_time, time;
 
+    const int items = 1E4;
     omp_set_num_threads(8);
+
+    nthreads = omp_get_num_threads();
+    int cas_misses_add[nthreads];
+    int cas_misses_find[nthreads];
+    int cas_misses_del[nthreads];
 
     #pragma omp parallel private(tid)
     {
-        tid = omp_get_thread_num();
-        printf("thread %d\n", tid);
+        int cas_miss_add = 0;
+        int cas_miss_find = 0;
+        int cas_miss_del = 0;
 
-        nthreads = omp_get_num_threads();
-        for(int i = (items*tid)/nthreads; i<(items*(tid+1))/nthreads; i++){
+        #pragma omp barrier
+        if(tid == 0) {
+            start_time = omp_get_wtime();
+        }
+
+
+        tid = omp_get_thread_num();
+        //printf("thread %d\n", tid);
+        
+        int i;
+        for(i = (items*tid)/nthreads; i<(items*(tid+1))/nthreads; i++){
             list->add(i);
         }
+
+
+        #pragma omp barrier
+        if(tid == 0) {
+            end_time = omp_get_wtime();
+            time = end_time - start_time;
+        }
+
+        cas_misses_add[tid] = cas_miss_add;
+        cas_misses_del[tid] = cas_miss_del;
+        cas_misses_find[tid] = cas_miss_del;
         
+    } //end parallel
+
+    int all_misses_add = 0;
+    int all_misses_find = 0;
+    int all_misses_del = 0;
+
+    for(int i=0; i<nthreads; i++) {
+        all_misses_add += cas_misses_add[i];
+        all_misses_find += cas_misses_find[i];
+        all_misses_del += cas_misses_del[i];
     }
-    list->print();
+
+    printf("CAS Misses\n \
+            ADD: %d, DEL: %d, FIND: %d\n", all_misses_add, all_misses_del, all_misses_find);
+    printf("Duration: %.3lf seconds\n", time);
+
+    //list->print();
 
     return 0;
 }
