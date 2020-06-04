c5b727253624c1d6b8fa46605159a7887b9b027a
sevjaeg <sev.jaeger@gmail.com>
2020-06-04 17:59:55 +0200

    wait-free finished, still bugs with remove
    Signed-off-by: sevjaeg <sev.jaeger@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,132 +6,93 @@
 #include "wait_free_list.cpp"
 #include "stamped_marked_pointer.h"
 
+#define LOCK_FREE 0
+#define WAIT_FREE 1
+
+#define OPERATIONAL 0
+#define CLEANUP 1
+
 int main(int argc, char *argv[])
 {
-    int nthreads, tid;
-    double start_time, end_time, time, time_wf;
+    // Experiment parameters
+    const int items_lf = 1E4;
+    const int items_wf = 1E2;
+    omp_set_num_threads(1);
 
-    /*
-    cout << "\nmarked" << "\n";
-    LockFreeNode<int> n(4);
-    LockFreeNode<int>* nptr;
-    nptr = &n;
+    double start_time, end_time;
+    int nthreads, tid;    
 
-    std::cout << nptr << "\n";
-    std::cout << getPointer(nptr) <<"\n";
-    std::cout << getFlag(nptr)<<"\n";
-
-    setFlag((void**)&nptr);
-    std::cout << nptr << "\n";
-    std::cout << getPointer(nptr)<<"\n";
-    std::cout << getFlag(nptr)<<"\n";
-
-    resetFlag((void**)&nptr);
-    std::cout << nptr << "\n";
-    std::cout << getFlag(nptr)<<"\n";
-    
-    
-    
-    cout << "\nstamped" << "\n";
-    WaitFreeNode<int> n2(4);
-    WaitFreeNode<int> *nptr2;
-    nptr2 = &n2;
-
-    std::cout << nptr2 << "\n";
-    std::cout << getPointer(nptr2) <<"\n";
-    std::cout << getStamp(nptr2)<<"\n";
-    std::cout << getFlag(nptr2)<<"\n";
-
-    uint16_t ui = 124;
-    setStamp((void**)&nptr2, ui);
-    setFlag((void**)&nptr2);
-    
-    std::cout << nptr2 << "\n";
-    std::cout << getPointer(nptr2)<<"\n";
-    std::cout << getStamp(nptr2)<<"\n";
-    std::cout << getFlag(nptr2)<<"\n";;
-
-    incrementStamp((void**)&nptr2);
-    resetFlag((void**)&nptr2);
-    
-    std::cout << nptr2 << "\n";
-    std::cout << getPointer(nptr2) <<"\n";
-    std::cout << getStamp(nptr2)<<"\n";
-    std::cout << getFlag(nptr2)<<"\n";
-
-    resetStamp((void**)&nptr2);
-    
-    std::cout << nptr2 << "\n";
-    std::cout << getPointer(nptr2) <<"\n";
-    std::cout << getStamp(nptr2)<<"\n";
-    std::cout << getFlag(nptr2)<<"\n";
-    
-    */
-
-    LockFreeList<int> *list = new LockFreeList<int>(INT32_MIN, INT32_MAX);
-    WaitFreeList<int> *wfList;
-
-    const int items = 1E3;
-    const int items_wf = 1E3;
-    omp_set_num_threads(8);
-
+#if LOCK_FREE
+    LockFreeList<int> *lfList = new LockFreeList<int>(INT32_MIN, INT32_MAX);
     int cas_misses_add[nthreads + 32];
     int cas_misses_del[nthreads + 32];
-   
+    double time_fill_lf, time_op_lf = 0, time_clean_lf = 0;
+
     #pragma omp parallel private(tid)
     {   
         nthreads = omp_get_num_threads();
         tid = omp_get_thread_num();
+
         int misses_add = 0;
         int misses_del = 0;
-
         if (tid == 0)
         {   
-            printf("\n_____________________\nLock-Free: Adding %d items using %d threads\n", items, nthreads);
+            printf("\n_____________________\nLock-Free: %d items, %d threads\n", items_lf, nthreads);
             start_time = omp_get_wtime();
         }
         #pragma omp barrier
 
-        int i;
-        for (i = (items * tid) / nthreads; i < (items * (tid + 1)) / nthreads; i++)
+        for (int i = (items_lf * tid) / nthreads; i < (items_lf * (tid + 1)) / nthreads; i++)
         {   
-            int misses =  list->add(i);
+            int misses =  lfList->add(i);
             misses_add += misses;
-            /*if(misses != 0) {
-                printf("miss @t%d,i=%d\n", tid, i);
-            }*/
+        }  
+        #pragma omp barrier
+        if (tid == 0)
+        {
+            end_time = omp_get_wtime();
+            time_fill_lf = end_time - start_time;
+            
         }
+
+        #if OPERATIONAL
+        if(tid == 0) {
+            printf("Operational Benchmark\n");
+            start_time = omp_get_wtime();    
+        }
+        #pragma omp barrier
+
+        //TODO operations
 
         #pragma omp barrier
         if (tid == 0)
         {
             end_time = omp_get_wtime();
-            time = end_time - start_time;
+            time_op_lf = end_time - start_time;
         }
+        #endif //operational
+
+        #if CLEANUP
+        if(tid == 0) {
+            printf("Removing all elements\n");
+            start_time = omp_get_wtime();
+        }
+        #pragma omp barrier
+        for (int i = (items_lf * tid) / nthreads; i < (items_lf * (tid + 1)) / nthreads; i++)
+        {   
+            int misses =  lfList->remove(i);
+            misses_add += misses;
+        }
+        #pragma omp barrier
+        if (tid == 0)
+        {
+            end_time = omp_get_wtime();
+            time_clean_lf = end_time - start_time;
+        }
+        #endif //cleanup
 
         cas_misses_add[tid] = misses_add;
         cas_misses_del[tid] = misses_del;
-
-        if (tid == 0) {
-            wfList = new WaitFreeList<int>(INT32_MIN, INT32_MAX, nthreads);
-            wfList->print();
-            printf("\n_____________________\nWait-Free: Adding %d items using %d threads\n", items_wf, nthreads);
-            start_time = omp_get_wtime();
-        }
-        #pragma omp barrier
-
-        for (i = (items_wf * tid) / nthreads; i < (items_wf * (tid + 1)) / nthreads; i++)
-        {   
-            wfList->add(tid, i);
-        }
-
-        #pragma omp barrier
-        if (tid == 0)
-        {
-            end_time = omp_get_wtime();
-            time_wf = end_time - start_time;
-        }
-
     } //end parallel
 
     printf("ADD: ");
@@ -146,8 +107,6 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
-
-
     int all_misses_add = 0;
     int all_misses_del = 0;
 
@@ -157,14 +116,84 @@ int main(int argc, char *argv[])
         all_misses_del += cas_misses_del[i];
     }
 
+    
+    printf("\nDuration Lock-Free: \nFill: %.3lf seconds\nOperation: %.3lf seconds\nCleanup: %.3lf seconds\n",
+        time_fill_lf, time_op_lf, time_clean_lf);
     printf("CAS Misses: ADD: %d, DEL: %d\n", 
            all_misses_add, all_misses_del);
-    printf("Duration Lock-Free: %.3lf seconds\n", time);
+    //lfList->print();
 
-    printf("Duration Wait-Free: %.3lf seconds\n", time_wf);
+#endif //lock-free benchmark
 
-    //list->print();
+#if WAIT_FREE
+    WaitFreeList<int> *wfList;
+    double time_fill_wf, time_op_wf = 0, time_clean_wf = 0;
+
+    #pragma omp parallel private(tid)
+    {   
+        nthreads = omp_get_num_threads();
+        tid = omp_get_thread_num();
+        if (tid == 0) {
+            wfList = new WaitFreeList<int>(INT32_MIN, INT32_MAX, nthreads);
+            printf("\n_____________________\nWait-Free: %d items, %d threads\n", items_wf, nthreads);
+            start_time = omp_get_wtime();
+        }
+        #pragma omp barrier
+
+        for (int i = (items_wf * tid) / nthreads; i < (items_wf * (tid + 1)) / nthreads; i++)
+        {   
+            wfList->add(tid, i);
+        }
+        #pragma omp barrier
+        if (tid == 0)
+        {
+            end_time = omp_get_wtime();
+            time_fill_wf = end_time - start_time;
+            
+        }
+
+        #if OPERATIONAL
+        if(tid == 0) {
+            printf("Operational Benchmark\n");
+            start_time = omp_get_wtime();    
+        }
+        #pragma omp barrier
+
+        //TODO operations
+
+        #pragma omp barrier
+        if (tid == 0)
+        {
+            end_time = omp_get_wtime();
+            time_op_wf = end_time - start_time;
+        }
+        #endif //operational
+
+        #if CLEANUP
+        if(tid == 0) {
+            printf("Removing all elements\n");
+            start_time = omp_get_wtime();
+        }
+        #pragma omp barrier
+        for (int i = (items_wf * tid) / nthreads; i < (items_wf * (tid + 1)) / nthreads; i++)
+        {   
+            //TODO does not work!
+            wfList->remove(tid, i);
+        }
+        #pragma omp barrier
+        if (tid == 0)
+        {
+            end_time = omp_get_wtime();
+            time_clean_wf = end_time - start_time;
+        }
+        #endif //cleanup
+    } // end parallel  
+
+    printf("\nDuration Wait-Free: \nFill: %.3lf seconds\nOperation: %.3lf seconds\nCleanup: %.3lf seconds\n",
+        time_fill_wf, time_op_wf, time_clean_wf);
     wfList->print();
+
+#endif //lock-free benchmark
 
     return 0;
 }
--- src/wait_free_list.cpp
+++ src/wait_free_list.cpp
@@ -5,6 +5,8 @@
 
 #include "stamped_marked_pointer.h"
 
+#define DEBUG 1
+
 using namespace std;
 
 enum OperationType{
@@ -56,7 +58,10 @@ class WaitFreeList {
      * Creates sentinels, Initialises state array 
      */
     public : WaitFreeList(T first_sent, T last_sent, int nthreads) {
+        #if DEBUG
         cout << "creating wait-free list" << "\n";
+        #endif
+
         first_sentinel_value = first_sent;
         last_sentinel_value = last_sent;
         current_max_phase = 0;
@@ -89,7 +94,10 @@ class WaitFreeList {
 
     // checked
     public: bool add(int tid, T value) {
+        #if DEBUG
         cout << "addding " << value << "\n";
+        #endif
+
         long phase = maxPhase();
         WaitFreeNode<T>* newNode = new WaitFreeNode<T>(value);
         OperationDescriptor<T>* op = new OperationDescriptor<T>(phase, OperationType::ADD, newNode, nullptr);
@@ -101,7 +109,10 @@ class WaitFreeList {
 
     //checked
     public : bool remove(int tid, T value) {
+        #if DEBUG
         cout << "removing " << value << "\n";
+        #endif
+
         if(value == first_sentinel_value || value == last_sentinel_value) {
             return false; //dont remove sentinels
         }
@@ -109,7 +120,7 @@ class WaitFreeList {
         WaitFreeNode<T>* newNode = new WaitFreeNode<T>(value);
         OperationDescriptor<T>* op = new OperationDescriptor<T>(phase, OperationType::SEARCH_REMOVE, newNode, nullptr);
         *state.at(tid) = op;
-        //help(phase);
+        help(phase);
         op = *state.at(tid);
         if(op->type == OperationType::DETERMINE_REMOVE) {
             bool a = false; // workaround
@@ -131,18 +142,25 @@ class WaitFreeList {
                 marked = getFlag(curr->next);
                 while(marked) {
                     //node logically deleted, remove it
-
                     snip = compareAndSet(pred->next, curr, succ, false, false);
 
                     if(!isSearchStillPending(tid, phase)) {
                         return nullptr;
                     }
                     if (!snip) goto retry;
+                    #if DEBUG
+                    cout << "Search removed node physically\n";
+                    #endif
+
+                    curr = succ;
                     succ = (WaitFreeNode<T>*)getPointer(curr->next);
                     marked = getFlag(curr->next);
                 }
                 if(curr->item >= value) {
+                    #if DEBUG
                     cout << "searched " << value << ", found window " << pred->item << "," << curr->item <<"\n";
+                    #endif
+
                     return new WaitFreeWindow<T>(pred, curr);
                 }
                 pred = curr;
@@ -155,13 +173,19 @@ class WaitFreeList {
     private: void help(long phase) {
         for(int i = 0; i < state_size; i++) {
             OperationDescriptor<T>* desc = *state.at(i);
-            cout << "state[" << i << "]: type:" << desc->type << " phase:" <<desc->phase<<"\n";
+            //cout << "state[" << i << "]: type:" << desc->type << " phase:" <<desc->phase<<"\n";
             if(desc->phase <= phase) { // help all pending operations
                 if(desc->type == OperationType::ADD) {
+                    #if DEBUG
                     cout << "helping add\n";
+                    #endif
+
                     helpAdd(i, desc->phase);
                 } else if(desc->type == OperationType::SEARCH_REMOVE || desc->type == OperationType::EXECUTE_REMOVE) {
+                    #if DEBUG
                     cout << "helping remove\n";
+                    #endif
+
                     helpRemove(i, desc->phase);
                 }
             }
@@ -172,45 +196,56 @@ class WaitFreeList {
         while(true) {
             OperationDescriptor<T>* op = *state.at(tid);
             if(!(op->type == OperationType::ADD && op->phase == phase)) {
+                #if DEBUG
+                cout << "add no longer relevant\n";
+                #endif
+
                 return;
             }
             WaitFreeNode<T>* node = op->node;
             WaitFreeNode<T>* next_node = (WaitFreeNode<T>*)getPointer(node->next);
             WaitFreeWindow<T>* window = search(tid, node->item, phase);
             if(window == nullptr) {
+                #if DEBUG
                 cout << "window == nullptr\n";
+                #endif
+
                 return;
             }
             if(window->curr->item == node->item) {
                 if((window->curr == node) || getFlag(node->next)) {
                     // node already inserted
+                    #if DEBUG
                     cout << "node already inserted before: success\n";
-                    
+                    #endif
+
                     OperationDescriptor<T>* success = new OperationDescriptor<T>(phase, OperationType::SUCCESS,
                         node, nullptr);
                     if((*state[tid]).compare_exchange_strong(op, success)) {
                         return;
                     }
                 } else {
+                    #if DEBUG
                     cout << "node not yet inserted: failure\n";
-                    return;
+                    #endif
                     
                     OperationDescriptor<T>* failure = new OperationDescriptor<T>(phase, OperationType::FAILURE,
                         node, nullptr);
                     if((*state[tid]).compare_exchange_strong(op, failure)) {
                         return;
                     }
-                    
                 } 
             } else {
                 if(getFlag(node->next)) {
-                    cout << "node already inserted and deleted\n";
+                    #if DEBUG
+                    cout << "node already inserted and deleted: success\n";
+                    #endif
+
                     OperationDescriptor<T>* success = new OperationDescriptor<T>(phase, OperationType::SUCCESS,
                         node, nullptr);
                     if((*state[tid]).compare_exchange_strong(op, success)) {
                         return;
                     }
-        
                 }
 
                 int version = getStamp(window->pred->next);
@@ -227,14 +262,73 @@ class WaitFreeList {
                         return;
                     }
                 }
-                
-                return;
             }
         }
     }
 
     private: void helpRemove(int tid, long phase) {
+        while(true) {
+            OperationDescriptor<T> *op = *state.at(tid);
+            if(!((op->type == OperationType::SEARCH_REMOVE || op->type == OperationType::EXECUTE_REMOVE)
+                && op->phase == phase)) {
+                    #if DEBUG
+                    cout << "remove no longer relevant\n";
+                    #endif
 
+                    return;
+            }
+            WaitFreeNode<T> *node = op->node;
+            if(op->type == OperationType::SEARCH_REMOVE) {
+                WaitFreeWindow<T> *window = search(tid, node->item, phase);
+                if(window == nullptr) {
+                    #if DEBUG
+                    cout << "window == nullptr\n";
+                    #endif
+
+                    continue;
+                }
+                if(window->curr->item != node->item) {
+                    #if DEBUG
+                    cout << "Searched item " << node->item << " not in list\n";
+                    #endif
+
+                    OperationDescriptor<T> *failure = new OperationDescriptor<T>(phase, OperationType::FAILURE, node, nullptr);
+                    if((*state[tid]).compare_exchange_strong(op, failure)) {
+                        return;
+                    }
+                } else {
+                    #if DEBUG
+                    cout << "found item, ready for execute\n";
+                    #endif
+
+                    OperationDescriptor<T> *found = new OperationDescriptor<T>(phase, OperationType::EXECUTE_REMOVE, node, window);
+                    (*state[tid]).compare_exchange_strong(op, found);
+                }
+            } else if(op->type == OperationType::EXECUTE_REMOVE) {
+                WaitFreeNode<T>* next = (WaitFreeNode<T>*)getPointer(op->search_result->curr->next);
+                #if DEBUG
+                cout << "Try to mark node\n";
+                #endif
+
+                if(!attemptFlag(op->search_result->curr->next, next, true)) {
+                     #if DEBUG
+                    cout << "Unable to mark, try again\n";
+                    #endif
+
+                    continue;
+                }
+                //removes node physically
+                search(tid, op->node->item, phase);
+                #if DEBUG
+                cout << "Node removed by search\n";
+                #endif
+
+                OperationDescriptor<T> *determine = new OperationDescriptor<T>(op->phase, OperationType::DETERMINE_REMOVE, op->node, op->search_result);
+                (*state[tid]).compare_exchange_strong(op, determine);
+                return;
+            }
+            
+        }
     }
 
     private : bool compareAndSet(atomic<WaitFreeNode<T>*> &current, WaitFreeNode<T>* expected, WaitFreeNode<T>* desired, bool expectedFlag, bool desiredFlag) {
@@ -265,6 +359,20 @@ class WaitFreeList {
             ((desired == (WaitFreeNode<T>*)getPointer(current_old) &&
             desiredFlag == getFlag(current_old)) ||
             current.compare_exchange_strong(current_old, desired_full));
+    }
+
+    private : bool attemptFlag(atomic<WaitFreeNode<T>*> &current, WaitFreeNode<T>* expected, bool newFlag) {
+        WaitFreeNode<T>* current_old = current;
+        WaitFreeNode<T>* desired_full = expected;
+        if(newFlag) {
+            setFlag((void **) desired_full);
+        }
+        setStamp((void **) desired_full, getStamp(current) + 1);
+
+        return expected == (WaitFreeNode<T>*)getPointer(current_old)
+            && (newFlag == getFlag(current_old) ||
+            current.compare_exchange_strong(current_old, desired_full));
+
     }
 
     private: long maxPhase() {
